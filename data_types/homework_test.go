4281e4575cddcafc800888ce13bc8b27ed75cf98
maxim <korobov.maksim6@wb.ru>
2025-04-09 12:51:42 +0300

    hw1

--- data_types/homework_test.go
+++ data_types/homework_test.go
@@ -9,10 +9,7 @@ import (
 // go test -v homework_test.go
 
 func ToLittleEndian(number uint32) uint32 {
-	return ((number >> 24) & 0xff) |
-		((number >> 8) & 0xff00) |
-		((number << 8) & 0xff0000) |
-		((number << 24) & 0xff000000)
+	return 0 // need to implement
 }
 
 func TestСonversion(t *testing.T) {
